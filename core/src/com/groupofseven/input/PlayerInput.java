67a098ce84bfe60b677b71cf81713d1fa36fbc48
TomokoK <PrivateTracker1941@gmail.com>
2017-06-11 16:01:43 -0400

    cleaned up some classes

--- core/src/com/groupofseven/input/PlayerInput.java
+++ core/src/com/groupofseven/input/PlayerInput.java
@@ -10,34 +10,27 @@ public class PlayerInput extends InputAdapter {
 		
 	public PlayerInput(Player p) {
 		this.player = p;
-		
-		//Player player = new Player(0, 0);
-	}
+		}
 	
+	//console output is fine, player doesn't move though.
 	@Override
 	public boolean keyDown(int keycode) {
-		//player = new Player(0, 0);
-		//pressing W A S or D crashes the program with a nullpointerexception
 		if (keycode == Keys.W) {
-			// *** UNCOMMENTING THIS WILL GIVE A NULLPOINTEREXCEPTION ***
 			this.player.move(0, 1);
 			System.out.println("PRESSING W");
 		}
 		
 		if (keycode == Keys.A){
-			// *** UNCOMMENTING THIS WILL GIVE A NULLPOINTEREXCEPTION ***
 			this.player.move(-1, 0);
 			System.out.println("PRESSING A");
 		}
 		
 		if (keycode == Keys.S) {
-			// *** UNCOMMENTING THIS WILL GIVE A NULLPOINTEREXCEPTION ***
 			this.player.move(0, -1);
 			System.out.println("PRESSING S");
 		}
 		
 		if(keycode == Keys.D) {
-			// *** UNCOMMENTING THIS WILL GIVE A NULLPOINTEREXCEPTION ***
 			this.player.move(1, 0);
 			System.out.println("PRESSING D");
 		}
