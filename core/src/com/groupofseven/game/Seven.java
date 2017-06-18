0875b652d9b291218d4b9414e7fc49c1c0bdc75e
TomokoK <PrivateTracker1941@gmail.com>
2017-06-18 14:13:20 -0400

    nullpointerexceptions

--- core/src/com/groupofseven/game/Seven.java
+++ core/src/com/groupofseven/game/Seven.java
@@ -33,7 +33,7 @@ public class Seven extends Game implements ApplicationListener {
 	public void create() {
 		map = new TmxMapLoader().load("maps/Class 1A.tmx");
 
-		Player me = new Player(this, (TiledMapTileLayer) map.getLayers().get(0));
+		me = new Player(this, (TiledMapTileLayer) map.getLayers().get(0));
 		//create a new spritebatch for the sprite
 		batch = new SpriteBatch();
 		
