817d0acf6f5fa69f38c36e13a40b32e07d4f1a47
TomokoK <PrivateTracker1941@gmail.com>
2017-11-24 00:03:32 -0500

    edited a few comments for clarity

--- core/src/com/groupofseven/game/Settings.java
+++ core/src/com/groupofseven/game/Settings.java
@@ -4,13 +4,13 @@ public class Settings {
 	// size of each map tile
 	public static final int TILE_SIZE = 24;
 
-	// stats on the sprite sheets
+	// define stats on the sprite sheets
 	public static final int SPRITE_ROWS = 4;
 	public static final int SPRITE_COLUMNS = 4;
 	public static final int SPRITE_PX_WIDTH = 128;
 	public static final int SPRITE_PX_HEIGHT = 192;
 
-	// stats on the rendered sprite size
+	// define stats on the rendered sprite size
 	public static final int SPRITE_HEIGHT = SPRITE_PX_HEIGHT / SPRITE_COLUMNS;
 	public static final int SPRITE_WIDTH = SPRITE_PX_WIDTH / SPRITE_ROWS;
 
