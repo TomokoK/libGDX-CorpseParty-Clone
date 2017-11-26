8a3d082b2abb3384f5b2d2d5c7f605f23b3aa566
TomokoK <PrivateTracker1941@gmail.com>
2017-11-26 14:40:23 -0500

    starting to add sprite animations

---
+++ core/src/com/groupofseven/model/Animator.java
@@ -0,0 +1,92 @@
+package com.groupofseven.model;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.groupofseven.game.Settings;
+
+public class Animator implements ApplicationListener {
+
+	// Objects here
+	Animation<TextureRegion> walkAnimation; // declare frame type (texture region)
+	Texture walkSheet;
+	SpriteBatch spriteBatch;
+	
+	// this float is used to track elapsed animation time
+	float stateTime;
+	
+	@Override
+	public void create() {
+		// Load sprite sheet as a texture
+		walkSheet = new Texture(Gdx.files.internal("maps/Ayumi.png"));
+
+		// Use the split utility method to create a 2D array of TextureRegions. This is 
+		// possible because this sprite sheet contains frames of equal size and they are 
+		// all aligned.
+		TextureRegion[][] tmp = TextureRegion.split(walkSheet, 
+				walkSheet.getWidth() / Settings.SPRITE_COLUMNS,
+				walkSheet.getHeight() / Settings.SPRITE_ROWS);
+		
+		// Place the regions into a 1D array in the correct order, starting from the top 
+		// left, going across first. The Animation constructor requires a 1D array.
+		TextureRegion[] walkFrames = new TextureRegion[Settings.SPRITE_COLUMNS * Settings.SPRITE_ROWS];
+		int index = 0;
+		for (int i = 0; i < Settings.SPRITE_ROWS; i++) {
+			for (int j = 0; j < Settings.SPRITE_COLUMNS; j++) {
+				walkFrames[index++] = tmp[i][j];
+			}
+		}
+
+		// Initialize the Animation with the frame interval and array of frames
+		walkAnimation = new Animation<TextureRegion>(0.025f, walkFrames);
+
+		// Instantiate a SpriteBatch for drawing and reset the elapsed animation
+		// time to 0
+		spriteBatch = new SpriteBatch();
+		stateTime = 0f;
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen
+		stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
+		
+		// Get current frame of animation for the current stateTime
+		TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+		spriteBatch.begin();
+		spriteBatch.draw(currentFrame, 50, 50); // Draw current frame at (50, 50)
+		spriteBatch.end();
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		spriteBatch.dispose();
+		walkSheet.dispose();
+		
+	}
+
+}
