8a796c8ec48bdcb23b5aace6aa819136d9e31500
TomokoK <PrivateTracker1941@gmail.com>
2017-11-26 00:41:46 -0500

    overhauled the class 1a map for less sprite clipping with objects, added a structure for the rest of the map conversions.

--- desktop/src/com/seven/game/desktop/DesktopLauncher.java
+++ desktop/src/com/seven/game/desktop/DesktopLauncher.java
@@ -4,10 +4,22 @@
  * 
  * TODO:
  * 	Add all maps
- * 		Test new img2tmx script for better col. detec. ?
+ * 		***NEW ADD MAP PROCESS***
+ * 		***ALL EXISTING MAPS EXCEPT FOR CLASS 1A MUST BE CONVERTED***
+ * 				1. Create a production environment for new map
+ * 				2. Calculate blackspace adjustments and # of tiles
+ * 				   with source map PNG
+ * 				3. Adjust source PNG, save to production environment
+ * 				4. cd into production environment
+ * 				5. Run 'node ~/.bin/Image2Map.js 24 24 [name_of_map].png'
+ * 				6. Edit resulting TMX file in tiled for collision detection,
+ * 				   objects, etc...
+ * 				7. Remove original tileset and tmx file from project assets,
+ * 				   replace with the new tileset and tmx file
+ * 		***END NEW PROCESS***
  * 	Add + improve collision detection
  * 	Add voice acting
- * 	Sprite animations
+ * 	Sprite animations *PRIORITY*
  * 	Main menu
  * 		Save/Load
  * 		Exit
@@ -17,6 +29,8 @@
  * 		Door collision detection -> change map
  * 		Pick up items, use them in areas
  * 	Change script from edited version -> original CP rip (?)
+ * 	Fix camera to be more authentic to the original (Load up a vita save and take a look)
+ * 	Tune the volume, fix the non loop in Class 1A
  * 
  */
 package com.seven.game.desktop;
